380bc4c2f3fac07a1e9eb451b23606f410d7da93
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-05-04 10:20:48 +0700

    before conflic

--- IoT-Devices/Valarien-Sight/src/main.cpp
+++ IoT-Devices/Valarien-Sight/src/main.cpp
@@ -1,51 +1,58 @@
 #include <Arduino.h>
 #include <esp_camera.h>
 #include <camera_pins.h>
-#include "esp_timer.h"
-#include "img_converters.h"
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-bool deviceConnected = false;
+// #include <BLEDevice.h>
+// #include <BLEServer.h>
+// #include <BLEUtils.h>
+// #include <BLE2902.h>
+#include <WiFi.h>
+const char *ssid = "Ambrose";
+const char *password = "12345678";
+
+WebServer server(80);
 camera_fb_t *fb = NULL;
-bool capture()
+void handlecapture()
 {
 	fb = esp_camera_fb_get();
 	if (!fb)
 	{
-		Serial.println("Camera capture failed");
-		return false;
+		Serial.println("Fail to captute");
 	}
-	return true;
+	server.send(200, "image/jpeg",(const char *)fb->buf);
+	// httpd_resp_send(req,(const char *)fb->buf,fb->len);
+	
+	esp_camera_fb_return(fb);
+	// server.sendContent((char *)*(fb->buf));
 }
-class MyServerCallbacks : public BLEServerCallbacks
-{
-	void onConnect(BLEServer *pServer)
-	{
-		Serial.println("BLE Client Connected");
-		Serial.println(pServer->getConnId());
-		deviceConnected = true;
-		BLEDevice::startAdvertising();
-	};
 
-	void onDisconnect(BLEServer *pServer)
-	{
-		deviceConnected = false;
-	}
-};
+void handleRoot()
+{
+	Serial.println("Run");
+	// server.send(200, "text/plain", "Hello form server");
+}
 
 void setup()
 {
 
 	Serial.begin(115200);
 	// pinMode(4,OUTPUT);
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	
+
+	// Wait for connection
+	uint8_t i = 0;
+	while (WiFi.status() != WL_CONNECTED && i++ < 20)
+	{ //wait 10 seconds
+		delay(500);
+	}
+
 	initCamera_Config();
+
 	delay(200);
 	esp_err_t err = esp_camera_init(&camera_config);
 	if (err != ESP_OK)
@@ -54,56 +61,24 @@ void setup()
 		return;
 	}
 
-	Serial.println("Begin Capture");
+	// if (MDNS.begin("ValerianSight"))
+	// {
+	// 	MDNS.addService("http", "tcp", 80);
+	// 	Serial.println("MDNS responder started");
+	// 	Serial.println("You can now connect to http://");
+	// 	Serial.print("ValerianSight");
+	// 	Serial.print(".local");
+	// }
+	server.on("/", handleRoot);
+	server.on("/capture", handlecapture);
+	server.begin();
 
-	if (capture())
-	{
-		Serial.println(fb->len);
-	}
-
-	esp_camera_fb_return(fb);
-
-	// BLE Section
-	BLEDevice::init("ESP32 Valarien Sight");
-
-	pServer = BLEDevice::createServer();
-
-	pServer->setCallbacks(new MyServerCallbacks());
-	// Create the BLE Service
-	BLEService *pService = pServer->createService(SERVICE_UUID);
-	// Create a BLE Characteristic
-	pCharacteristic = pService->createCharacteristic(
-		CHARACTERISTIC_UUID,
-		BLECharacteristic::PROPERTY_READ |
-			BLECharacteristic::PROPERTY_WRITE |
-			BLECharacteristic::PROPERTY_NOTIFY |
-			BLECharacteristic::PROPERTY_INDICATE);
-	pCharacteristic->addDescriptor(new BLE2902());
-
-	BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-	pAdvertising->addServiceUUID(SERVICE_UUID);
-	pAdvertising->setScanResponse(false);
-	pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
-	BLEDevice::startAdvertising();
-	Serial.println("Waiting a client connection to notify...");
+	Serial.print("Server ready on:");
+	// Serial.println(Wifi.)
+	Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
-	if (deviceConnected)
-	{
-		if (capture())
-		{
-			pCharacteristic->setValue(fb->buf, fb->len);
-			pCharacteristic->notify();
-		}
-		delay(500);
-		esp_camera_fb_return(fb);
-
-		// pCharacteristic->
-	}
-	// digitalWrite(4, HIGH);
-	// delay(500);
-	// digitalWrite(4, LOW);
-	// delay(500);
+	server.handleClient();
 }
