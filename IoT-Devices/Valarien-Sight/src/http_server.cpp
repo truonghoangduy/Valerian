34b47bcb0593d84ebf7eabcf64a2af613fb9fc60
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-05-27 01:47:56 +0700

    mDNS request send back the IpAdd

--- IoT-Devices/Valarien-Sight/src/http_server.cpp
+++ IoT-Devices/Valarien-Sight/src/http_server.cpp
@@ -2,8 +2,10 @@
 #include "esp_camera.h"
 #include "Arduino.h"
 #include <http_server_linker.h>
+#include <WiFi.h>
 // httpd_handle_t capture_httpd = NULL;
 camera_fb_t *fb = NULL;
+httpd_handle_t capture_httpd = NULL;
 
 static esp_err_t capture_handler(httpd_req_t *req)
 {
@@ -21,6 +23,7 @@ static esp_err_t capture_handler(httpd_req_t *req)
     }
 
     res_flag = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+    Serial.println("Sended Img");
 
     // free memory
     esp_camera_fb_return(fb);
@@ -30,10 +33,34 @@ static esp_err_t capture_handler(httpd_req_t *req)
     return res_flag;
 }
 
-httpd_handle_t  startCameraServer()
+static esp_err_t defaut_uri_handler(httpd_req_t *req)
+{
+    Serial.println("Defualt web ///");
+    esp_err_t res_flag = ESP_OK;
+    const char *ipInformation = WiFi.localIP().toString().c_str();
+    httpd_resp_set_type(req, "text/html");
+    // httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    res_flag = httpd_resp_send(req,(char *)ipInformation, strlen(ipInformation));
+    //192.168.1.7
+    if (res_flag == ESP_OK)
+    {
+        Serial.println("Sended");
+    }
+    
+
+    return res_flag;
+}
+
+void startCameraServer()
 {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
+    httpd_uri_t defaut_uri = {
+        .uri = "/",
+        .method = HTTP_GET,
+        .handler = defaut_uri_handler,
+        .user_ctx = NULL};
     httpd_uri_t capture_uri = {
         .uri = "/capture",
         .method = HTTP_GET,
@@ -41,14 +68,19 @@ httpd_handle_t  startCameraServer()
         .user_ctx = NULL};
 
     config.server_port = 80;
-    httpd_handle_t capture_httpd = NULL;
 
     if (httpd_start(&capture_httpd, &config) == ESP_OK)
     {
+        Serial.println("Init Webserver");
         /* code */
+        httpd_register_uri_handler(capture_httpd, &defaut_uri);
         httpd_register_uri_handler(capture_httpd, &capture_uri);
     }
-    return capture_httpd;
+    else
+    {
+        Serial.println("Fail To Init Webserver");
+    }
+    // return capture_httpd;
 }
 
 void deInitCameraServer(httpd_handle_t server)
@@ -58,7 +90,6 @@ void deInitCameraServer(httpd_handle_t server)
         httpd_stop(server);
         server = NULL;
     }
-   
 }
 
 void stopcamServer()
